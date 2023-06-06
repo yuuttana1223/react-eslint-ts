3e9f9f5f9527bb1a3e78fc628a46ab8971f98068
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2023-06-07 00:56:40 +0900

    🔧 react/recommendedの設定を追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "prettier",
