50391a385b3c6e0c33ef953bd8e380cffb22706c
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2023-06-09 18:46:13 +0900

    ♻️ containerからrootに変更

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,13 +2,13 @@ import { App } from "@components/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const container = document.getElementById("root");
+const root = document.getElementById("root");
 
-if (!container) {
+if (!root) {
   throw new Error("No root element found.");
 }
 
-ReactDOM.createRoot(container).render(
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
