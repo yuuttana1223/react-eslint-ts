904eb2e74f4ba9ccef887be0513c0e3ccecca688
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2023-06-09 19:06:17 +0900

    🔧 パスの指定を@/から始まるものに変更

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,7 @@
-import { App } from "@components/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { App } from "@/components/App";
 
 const root = document.getElementById("root");
 
